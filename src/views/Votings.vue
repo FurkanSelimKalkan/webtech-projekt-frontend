119d39cbf937ae237a89f2ab108fdaa9cebd00b5
FurkanSelimKalkan <furkan.selim1999@hotmail.com>
2022-05-27 22:27:42 +0200

    Votings html

--- src/views/Votings.vue
+++ src/views/Votings.vue
@@ -1,5 +1,25 @@
 <template>
 <h1>Welcome to Votings</h1>
+  <div class="row row-cols-1 row-cols-md-2 g-4">
+    <div class="col">
+      <div class="card">
+        <img src="../assets/outfit1.png" class="card-img-top" alt="Outfit1">
+        <div class="card-body">
+          <h5 class="card-title">This</h5>
+          <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <div class="card">
+        <img src="../assets/outfit2.png" class="card-img-top" alt="Outfit2">
+        <div class="card-body">
+          <h5 class="card-title">That</h5>
+          <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
