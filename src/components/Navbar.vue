0395e3ec6ee3486d13194839260d8fea26e28e99
chrslei <chrscao1@gmail.com>
2022-05-29 00:26:34 +0200

    - navbar sticks to top - jquery script implementiert (bootstrap)

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,27 +1,33 @@
 <template>
-  <nav class="navbar navbar-expand-lg bg-light">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#">This or That</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div class="navbar-nav">
+  <nav class="navbar sticky-top  navbar-light navbar-expand-sm bg-light">
+    <a class="navbar-brand" href="#">This or That</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
           <router-link class="nav-link" to="/">Home</router-link>
+        </li>
+        <li class="nav-item">
           <router-link class="nav-link" to="/about">About</router-link>
+        </li>
+        <li class="nav-item">
           <router-link class="nav-link" to="/votings">Votings</router-link>
-        </div>
-      </div>
+        </li>
+      </ul>
     </div>
   </nav>
+
 </template>
 
 <script>
 export default {
   name: 'Navbar'
 }
+
 </script>
 
 <style scoped>
-
 </style>
--- src/views/Votings.vue
+++ src/views/Votings.vue
@@ -1,16 +1,18 @@
 <template>
-<h1>Welcome to Votings</h1>
-  <div class="row row-cols-1 row-cols-md-2 g-4">
-    <div class="col" v-for="voting in votings" :key="voting.id">
-      <div class="card">
-        <img :src="getAvatar(voting)" class="card-img-top" :alt=" voting.title">
-        <div class="card-body">
-          <h5 class="card-title">{{ voting.title }}</h5>
-          <p class="card-text">Button</p>
+  <div class="gimmespace">
+    <div class="body"><div class="row row-cols-1 row-cols-md-2 g-4">
+      <div class="col" v-for="voting in votings" :key="voting.id">
+        <div class="card">
+          <img :src="getAvatar(voting)" class="card-img-top" :alt=" voting.title">
+          <div class="card-body">
+            <h5 class="card-title">{{ voting.title }}</h5>
+            <p class="card-text">Button</p>
+          </div>
         </div>
       </div>
+    </div></div>
   </div>
-  </div>
+
 </template>
 
 <script>
@@ -25,8 +27,13 @@ export default {
     getAvatar (voting) {
       if (voting.image1 === 'Outfit1.url') {
         return require('../assets/outfit1.png')
-      } else if (voting.image1 === 'Outfit2.url') {
+      } else if (voting.image2 === 'Outfit2.url') {
         return require('../assets/outfit2.png')
+      }
+    },
+    getSecondImage (voting) {
+      if (voting.image1 === 'Outfit1.url') {
+        return require('../assets/outfit1.png')
       }
     }
   },
@@ -48,5 +55,9 @@ export default {
 </script>
 
 <style scoped>
+.gimmespace {
+ position: relative;
+  top: 10px;
+}
 
 </style>
