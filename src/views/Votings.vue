177b39fa47571ac3f26acb9075a72db505a6ebbf
FurkanSelimKalkan <furkan.selim1999@hotmail.com>
2022-05-28 12:24:28 +0200

    implemented backend-frontend connection

--- src/views/Votings.vue
+++ src/views/Votings.vue
@@ -1,30 +1,49 @@
 <template>
 <h1>Welcome to Votings</h1>
   <div class="row row-cols-1 row-cols-md-2 g-4">
-    <div class="col">
+    <div class="col" v-for="voting in votings" :key="voting.id">
       <div class="card">
-        <img src="../assets/outfit1.png" class="card-img-top" alt="Outfit1">
+        <img :src="getAvatar(voting)" class="card-img-top" :alt=" voting.title">
         <div class="card-body">
-          <h5 class="card-title">This</h5>
+          <h5 class="card-title">{{ voting.title }}</h5>
           <p class="card-text">Button</p>
         </div>
       </div>
-    </div>
-    <div class="col">
-      <div class="card">
-        <img src="../assets/outfit2.png" class="card-img-top" alt="Outfit2">
-        <div class="card-body">
-          <h5 class="card-title">That</h5>
-          <p class="card-text">Button</p>
-        </div>
-      </div>
-    </div>
+  </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Votings'
+  name: 'Votings',
+  data () {
+    return {
+      votings: []
+    }
+  },
+  methods: {
+    getAvatar (voting) {
+      if (voting.image1 === 'Outfit1.url') {
+        return require('../assets/outfit1.png')
+      } else if (voting.image1 === 'Outfit2.url') {
+        return require('../assets/outfit2.png')
+      }
+    }
+  },
+  mounted () {
+    const endpoint = process.env.VUE_APP_BACKEND_BASE_URL + '/api/v1/votings'
+    const requstOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    }
+
+    fetch(endpoint, requstOptions)
+      .then(response => response.json())
+      .then(result => result.forEach(voting => {
+        this.votings.push(voting)
+      }))
+      .catch(error => console.log('error', error))
+  }
 }
 </script>
 
