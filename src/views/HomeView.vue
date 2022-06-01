11392a91273d299b8c5edb797c058a844250eefc
FurkanSelimKalkan <furkan.selim1999@hotmail.com>
2022-06-01 22:29:00 +0200

    Changed Logo name

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,11 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <img alt="App logo" src="../assets/logo.png">
     <HelloWorld msg="Hello World!"/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
