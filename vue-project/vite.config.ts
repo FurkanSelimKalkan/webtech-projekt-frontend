a3f46af2851017ce5281aae9d30b734c1d2c1bca
chrslei <chrscao1@gmail.com>
2022-06-03 14:56:57 +0200

    - fix

--- vue-project/vite.config.ts
+++ vue-project/vite.config.ts
@@ -1,6 +1,6 @@
-import {fileURLToPath, URL} from 'url'
+import { fileURLToPath, URL } from 'url'
 
-import {defineConfig} from 'vite'
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
