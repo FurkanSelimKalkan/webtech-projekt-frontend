6a60f44b8cc8fac91b083e626070fa249f5d6248
chrslei <chrscao1@gmail.com>
2022-06-18 15:54:07 +0200

    - votingCreateForm bugfix

--- src/components/VotingCreateForm.vue
+++ src/components/VotingCreateForm.vue
@@ -78,7 +78,18 @@ export default {
       image1: '',
       image2: '',
       text: String,
-      colot: String
+      colot: String,
+      profile: {}
+    }
+  },
+  created () {
+    if (this.auth.userProfile) {
+      this.profile = this.auth.userProfile
+    } else {
+      this.auth.getProfile((err, profile) => {
+        if (err) return console.log(err)
+        this.profile = profile
+      })
     }
   },
   methods: {
@@ -172,7 +183,7 @@ export default {
           title: this.title,
           image1: this.image1,
           image2: this.image2,
-          ownerId: this.user.sub
+          ownerId: this.profile.sub
         })
 
         const requestOptions = {
